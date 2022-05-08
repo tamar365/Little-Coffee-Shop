5743e74d9a9bd028ec36866c9865ed3b270c94f4
tamar365 <tamar.learningd@gmail.com>
2022-05-08 10:17:46 +0300

    upload

--- littleCoffeeShop.js
+++ littleCoffeeShop.js
@@ -103,3 +103,13 @@ function updateTotal() {
   document.getElementsByClassName("priceOfCartTotal")[0].innerText =
     total + "$";
 }
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginRight = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginRight= "0";
+}
